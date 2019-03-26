4685aab4822c0dd2dea53fab8ce08baa96cd809e
Ucizi Mafeni <ucizi.mafeni@turintech.ai>
2019-03-26 11:21:23 +0000

    add comment

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -353,7 +353,7 @@ clientkeys = gears.table.join(
     awful.key({ modkey, "Shift" }, "o",      function (c) c:move_to_screen()               end,
               {description = "move to screen", group = "client"}),
     awful.key({ modkey, }, "o",      function () awful.screen.focus_relative( 1) end,
-      {description = "move to screen", group = "client"}),
+      {description = "switch screen", group = "client"}),
     awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
               {description = "toggle keep on top", group = "client"}),
     awful.key({ modkey,           }, "n",
