eeec2e2ccde6010988643b708660401bf0f78f00
Ucizi Mafeni <ucizi.mafeni@turintech.ai>
2019-03-28 14:11:04 +0000

    add make-dir-locals script

---
+++ scripts/make_dir_locals.sh
@@ -0,0 +1,10 @@
+here=$(pwd)
+echo "((nil .
+  ((cmake-ide-project-dir . \"$here/\")
+   (cmake-ide-build-dir . \"$here/cmake-ide-build\")
+   (cmake-ide-cmake-opts . \"-DCMAKE_BUILD_TYPE=Debug\")
+   (helm-make-build-dir . \"cmake-ide-build\")
+   (helm-make-arguments . \"-j4\")
+   (helm-ctest-dir . \"$here/build/cmake-ide-build\")
+   (gtest-target . \"$here/OUT/bin/unit_tests\")
+  )))" >> .dir-locals.el
