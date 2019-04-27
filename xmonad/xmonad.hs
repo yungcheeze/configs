de5cdb7f671c34f1417c94a305d6b2138dfe1cd0
Ucizi Mafeni <ucizim@gmail.com>
2019-04-27 22:03:04 +0100

    basic keybindings

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,9 @@
+--Base
 import XMonad
+import System.Exit (exitSuccess)
 
 -- Utilities
-import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.EZConfig (additionalKeysP, removeKeysP)
 
 -- Layouts Modifiers
 import XMonad.Layout.Renamed (renamed, Rename(CutWordsLeft, Replace))
@@ -15,29 +17,70 @@ import XMonad.Layout.TwoPane
 
 -- Hooks
 import XMonad.Hooks.ManageDocks (avoidStruts)
+
+-- Actions
+import XMonad.Actions.CycleWS (nextScreen, shiftNextScreen)
+import XMonad.Actions.WindowBringer (gotoMenu)
+
 ------------------------------------------------------------------------
 -- General:
 myTerminal = "st -f 'MesloLGM Nerd Font Mono:size=11'"
+myBrowser = "chromium"
+myEditor = "emacsclient -c -a emacs"
 myModMask = mod4Mask
 myBorderWidth = 1
 
 ------------------------------------------------------------------------
 -- Layouts:
-mySpacing = 0
+mySpacing = 2
+noSpacing = 0
 
 myLayoutHook = avoidStruts $ myLayouts
 
-myLayouts = myFull ||| myTwoPane ||| myTile
+myLayouts = myFull ||| myTwoPane
 
 
 
 myTile = renamed [Replace "Tiled"] $ spacing mySpacing $ Tall 1 (3/100) (1/2)
-myFull = renamed [Replace "Full"] $ spacing 0 $ noBorders Full
-myTwoPane = renamed [Replace "TwoPane"] $ spacing 1 $ TwoPane (3/100) (1/2)
+myFull = renamed [Replace "Full"] $ spacing noSpacing $ noBorders Full
+myTwoPane = renamed [Replace "TwoPane"] $ spacing mySpacing $ TwoPane (3/100) (1/2)
 
-main = xmonad def
-    { terminal    = myTerminal
-    , modMask     = mod4Mask
-    , borderWidth = 1
-    , layoutHook = myLayoutHook
-    }
+------------------------------------------------------------------------
+-- Keys:
+myKeys =
+  [ ("M-S-r", spawn "xmonad --recompile; xmonad --restart")
+  , ("M-<Return>", spawn myTerminal)
+  , ("M-x", spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+  , ("M-<Esc>", io exitSuccess)
+  , ("M-c", spawn myBrowser)
+  , ("M-S-e", spawn myEditor)
+  , ("M-o", nextScreen)
+  , ("M-S-o", shiftNextScreen)
+  , ("M-S-q", kill)
+  , ("M-w", gotoMenu)
+  ]
+
+------------------------------------------------------------------------
+-- Keys:
+removedKeys =
+  [ "M-q" -- restart
+  , "M-S-q" -- quit
+  , "M-S-c" -- close window
+  , "M-<Tab>" -- cycle window forward
+  , "M-S-<Tab>" -- cycle window backward
+  , "M-p" -- dmenu
+  , "M-S-p" -- dmenu
+  , "M-S-w" , "M-S-e" , "M-S-r" -- move window to monitor
+  , "M-w" , "M-e" , "M-r" -- switch to monitor
+  ]
+
+myConfig = def
+  { terminal    = myTerminal
+  , modMask     = mod4Mask
+  , borderWidth = 1
+  , layoutHook = myLayoutHook
+  }
+
+main = xmonad $ myConfig
+  `removeKeysP` removedKeys
+  `additionalKeysP` myKeys
