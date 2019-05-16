78632d5d970ab9a82d895370d04d2425d7600051
Ucizi Mafeni <ucizi.mafeni@turintech.ai>
2019-05-16 09:15:34 +0100

    update start-up hook

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -38,7 +38,7 @@ myEditor = "emacsclient -c -a emacs"
 myLauncher = myConfigsDir ++ "/scripts/dmenu_recency"
 myConfigsDir = "/home/ucizi/configs"
 myWindowSwitcher = "rofi -show window -theme sidetab"
-myStatusBar = myConfigsDir ++ "/config/polybar/launch.sh &"
+myStatusBar = myConfigsDir ++ "/config/polybar/launch.sh"
 myWallpaperCmd = "/home/ucizi/.fehbg"
 myModMask = mod4Mask
 
@@ -162,13 +162,13 @@ removedKeys =
 myStartupHook = do
   setWMName "LG3D" -- hack to make Java GUI apps work. Xmonad isn't on the whitelist (-_-)
   setupMyWSGroups
-  spawnOnce myStatusBar
+  spawnOnce "dropbox start"
+  spawnOnce "xbindkeys"
   spawnOnce "redshift &"
   spawnOnce "compton -b &"
-  spawnOnce "xbindkeys"
-  spawnOnce "dropbox start"
   spawnOnce "workrave &"
   spawnOnce myWallpaperCmd
+  spawnOnce (myStatusBar ++ " &")
 
 ------------------------------------------------------------------------
 -- Main:
